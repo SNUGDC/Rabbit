f709c0f7057c2801c02e8f7a658e6c5fb7c0c638
Kais-DkM <kais0903@gmail.com>
2014-07-25 05:45:01 +0900

    added comments

--- Assets/Rabbit.cs
+++ Assets/Rabbit.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 
 public class Rabbit : MonoBehaviour {
 	
+	/*-----public data types-----*/
 	public enum Gender {MALE, FEMALE};
 	
+	/*-----public static variables-----*/
 	public static List<Rabbit> rabbitList = new List<Rabbit>();
-	public static readonly ulong startHunger = 200;
-	public static readonly ulong maxHunger = 1000;
+	public static readonly ulong startHunger = 200; // starting point of hungry state
+	public static readonly ulong maxHunger = 1000; // hunger for starving to death
 	public static Sprite sprMaleRabbitStand;
 	public static Sprite sprFemaleRabbitStand;
 	public static Sprite sprRabbitHold;
@@ -18,6 +20,7 @@ public class Rabbit : MonoBehaviour {
 	public static Sprite sprSmallJump;
 	public static Sprite sprSmallLand;
 	
+	/*-----public member variables-----*/
 	public bool grow{
 		get{
 			return mGrow;
@@ -63,18 +66,20 @@ public class Rabbit : MonoBehaviour {
 		}
 	}
 	
+	/*-----private member variables-----*/
 	private bool mGrow = false;
 	private bool mSelected = false;
-	private uint mFrameCounter = 0;
+	private uint mFrameCounter = 0; // for Jump Loop
 	private ulong mHunger = 0;
 	private ulong mLife;
 	private int mId;
-	private uint mJumpPeriod = 15;
-	private Vector3 mMovingDir;
+	private uint mJumpPeriod = 15; // for Jump Loop
+	private Vector3 mMovingDir; // for Jump Loop
 	private Gender mGender;
 	private Color mColor = new Color(1, 1, 1);
 	private List<Gene> mGeneList = new List<Gene>();
 	
+	/*-----public static functions-----*/
 	public static void init(){
 		sprMaleRabbitStand = Resources.LoadAll<Sprite>("txtrRabbit")[5];
 		sprFemaleRabbitStand = Resources.LoadAll<Sprite>("txtrRabbit")[1];
@@ -113,6 +118,7 @@ public class Rabbit : MonoBehaviour {
 		DestroyImmediate(target.gameObject);
 	}
 	
+	/*-----public member functions-----*/
 	// Use this for initialization
 	void Start () {
 		mId = rabbitList.Count;
@@ -123,16 +129,16 @@ public class Rabbit : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(++mHunger > maxHunger){
-			//rabbit starved to death
+			// rabbit starved to death
 			Rabbit.delete(this);
 			return;
 		}
 		if(mHunger > startHunger){
-			//rabbit is hungry
+			// rabbit is hungry
 			renderer.material.color = new Color(0.192f, 0.376f, 0.2f);
 			mJumpPeriod = 5;
 		}
-		//Rabbit Jump Loop
+		// Rabbit Jump Loop
 		if (!mSelected) {
 			if(mFrameCounter == 0){
 				if(!mGrow){
@@ -143,9 +149,9 @@ public class Rabbit : MonoBehaviour {
 				}
 			}
 			else if(mFrameCounter == mJumpPeriod * 4){
-				//find Carrot
+				// find Carrot
 				Carrot nearCarrot = FarmFunc.findNearCarrot(transform.position.x, transform.position.y);
-				//set new MovingDir
+				// set new MovingDir
 				if(nearCarrot && mHunger > startHunger){
 					mMovingDir = new Vector3 (nearCarrot.gameObject.transform.position.x - transform.position.x,
 					                          nearCarrot.gameObject.transform.position.y - transform.position.y);
@@ -155,7 +161,7 @@ public class Rabbit : MonoBehaviour {
 				else{
 					mMovingDir = new Vector3 (Random.Range (-10, 10), Random.Range (-10, 10), 0);
 				}
-				//boundary check
+				// boundary check
 				Vector3 maxLeftBottom = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
 				Vector3 maxRightTop = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.9f, Screen.height * 0.9f, 0));
 				if(mMovingDir.x + transform.position.x < maxLeftBottom.x || mMovingDir.x + transform.position.x > maxRightTop.x){
@@ -164,9 +170,9 @@ public class Rabbit : MonoBehaviour {
 				if(mMovingDir.y + transform.position.y < maxLeftBottom.y || mMovingDir.y + transform.position.y > maxRightTop.y){
 					mMovingDir.y *= -1;
 				}
-				//move position
+				// move position
 				transform.position += new Vector3(mMovingDir.x / 2, mMovingDir.y / 2, 0);
-				//change sprite
+				// change sprite
 				GetComponent<SpriteRenderer>().sprite = (mGrow) ? sprRabbitJump : sprSmallJump;
 			}
 			else if(mFrameCounter == mJumpPeriod * 5){
@@ -184,7 +190,7 @@ public class Rabbit : MonoBehaviour {
 	void OnMouseDrag(){
 		if (selected) {
 			Vector2 temp = Input.mousePosition;
-			//limit draggable area
+			// limit draggable area
 			temp.x = Mathf.Max(temp.x, 0);
 			temp.x = Mathf.Min(temp.x, scriptFarm.sWidth * 0.9f);
 			temp.y = Mathf.Max(temp.y, 0);
@@ -194,11 +200,13 @@ public class Rabbit : MonoBehaviour {
 	}
 
 	void OnTriggerStay2D(Collider2D collider){
+		// collide with carrot
 		if(collider.gameObject.tag == "carrot" && !mSelected && mHunger > startHunger){
 			scriptFarm.carrotList.Remove((collider.gameObject.GetComponent<Carrot>()));
 			Destroy(collider.gameObject);
 			mHunger = 0;
 			mJumpPeriod = 15;
+			// make rabbit grow
 			if(!mGrow){
 				mGrow = true;
 				mColor = mGeneList[1].Phenotype<Color>(new Color(0, 0, 0), delegate(Color arg1, Color arg2){return arg1 + arg2;}, delegate(Color arg1, int arg2){return arg1 / arg2;});
--- Assets/scriptFarm.cs
+++ Assets/scriptFarm.cs
@@ -5,8 +5,10 @@ using LitJson;
 
 public class scriptFarm : MonoBehaviour {
 
+	/*-----public data types-----*/
 	public enum GameState {GAME, DICT, HELP};
 	
+	/*-----public static variables-----*/
 	public static GameObject objRabbit;
 	public static GameObject objCarrot;
 	public static int sWidth{
@@ -25,6 +27,7 @@ public class scriptFarm : MonoBehaviour {
 		}
 	}
 	
+	/*-----private static variables-----*/
 	private static bool mTestMode = false;
 	private static bool mShowPopup = false;
 	private static int mMoney = 1100;
@@ -37,6 +40,7 @@ public class scriptFarm : MonoBehaviour {
 	private static GameState mCurState = GameState.GAME;
 	private static Rabbit mTargetRabbit = null;
 	
+	/*-----public static functions-----*/
 	// find gameobject at mouse position with condition
 	public static GameObject clickedObject(string tag, System.Func<GameObject, bool> condition){
 		Vector2 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -51,6 +55,7 @@ public class scriptFarm : MonoBehaviour {
 		return result;
 	}
 
+	/*-----public member functions-----*/
 	void Start () {
 		objRabbit = (GameObject)Resources.Load("prefabRabbit");
 		objCarrot = (GameObject)Resources.Load("prefabCarrot");
@@ -68,7 +73,7 @@ public class scriptFarm : MonoBehaviour {
 	
 	void Update () {
 		if(Input.GetMouseButtonDown(0)){
-			//select rabbit
+			// select rabbit
 			GameObject clicked = clickedObject("rabbit", delegate(GameObject input){return true;});
 			if(clicked != null){
 				mTargetRabbit = clicked.GetComponent<Rabbit>();
@@ -77,7 +82,7 @@ public class scriptFarm : MonoBehaviour {
 			mShowPopup = (mTargetRabbit != null);
 		}
 		if(Input.GetMouseButtonDown(1)){
-			//create carrot
+			// create carrot
 			if(Input.mousePosition.x <= mSWidth * 0.9f && Input.mousePosition.y <= mSHeight * 0.9f){
 				mCarrotList.Add(FarmFunc.createCarrot(Input.mousePosition.x, Input.mousePosition.y));
 			}
@@ -87,12 +92,12 @@ public class scriptFarm : MonoBehaviour {
 				mTargetRabbit.selected = false;
 				GameObject clicked = clickedObject("rabbit", delegate(GameObject input){return input.GetComponent<Rabbit>() != mTargetRabbit;});
 				Rabbit anotherRabbit = (clicked == null) ? null : clicked.GetComponent<Rabbit>();
-				//in trash area
+				// in trash area
 				if(Input.mousePosition.x > mSWidth * 0.9f && Input.mousePosition.y < mSHeight * 0.1f){
 					Rabbit.delete(mTargetRabbit);
 					mMoney += 200;
 				}
-				//found rabbit with different gender & both are grown
+				// found rabbit with different gender & both are grown
 				else if(anotherRabbit != null && anotherRabbit.gender != mTargetRabbit.gender
 					 && anotherRabbit.grow && mTargetRabbit.grow){
 					if(mMoney >= 100 || mTestMode){
@@ -110,40 +115,40 @@ public class scriptFarm : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		//money - text
+		// money - text
 		GUI.Label (new Rect (mSWidth * 0.05f, mSHeight * 0.13f, mSWidth * 0.1f, mSHeight * 0.1f), "money : " + mMoney.ToString());
-		//test mode - text
+		// test mode - text
 		if(mTestMode){
 			GUI.Label (new Rect(mSWidth * 0.05f, mSHeight * 0.16f, mSWidth * 0.1f, mSHeight * 0.1f), "test mode");
 		}
-		//trash
+		// trash
 		GUI.Label (new Rect (mSWidth * 0.9f, mSHeight * 0.9f, mSWidth * 0.1f, mSHeight * 0.1f), "trash", mPopupStyle);
-		//return button	
+		// return button	
 		if (GUI.Button (new Rect (mSWidth * 0.0f, mSHeight * 0.0f, mSWidth * 0.1f, mSHeight * 0.1f), "Return") && (mCurState == GameState.GAME)) {
 			Application.LoadLevel("sceneMainMenu");
 		}
-		//dict button
+		// dict button
 		if (GUI.Button (new Rect (mSWidth * 0.1f, mSHeight * 0.0f, mSWidth * 0.1f, mSHeight * 0.1f), "Dictionary") && (mCurState == GameState.GAME)) {
 			mCurState = GameState.DICT;
 			Time.timeScale = 0; // stop game time
 		}
-		//help button
+		// help button
 		if (GUI.Button (new Rect (mSWidth * 0.2f, mSHeight * 0.0f, mSWidth * 0.1f, mSHeight * 0.1f), "Help") && (mCurState == GameState.GAME)) {
 			mCurState = GameState.HELP;
 			Time.timeScale = 0; // stop game time
 		}
-		//buy button
+		// buy button
 		if (GUI.Button (new Rect (mSWidth * 0.3f, mSHeight * 0.0f, mSWidth * 0.1f, mSHeight * 0.1f), "Buy") && (mCurState == GameState.GAME)) {
 			if(mMoney >= 200 || mTestMode){
 				Rabbit.create(null, null);
 				mMoney -= 200;
 			}
 		}
-		//test mode button
+		// test mode button
 		if(GUI.Button (new Rect (mSWidth * 0.4f, mSHeight * 0.0f, mSWidth * 0.1f, mSHeight * 0.1f), "Test Mode") && (mCurState == GameState.GAME)){
 			mTestMode = !mTestMode;
 		}
-		//in dict mode
+		// in dict mode
 		if (mCurState == GameState.DICT) {
 			GUI.Label(new Rect(mSWidth * 0.1f, mSHeight * 0.1f, mSWidth * 0.8f, mSHeight * 0.8f), "Dictionary", mDictStyle);
 			if(GUI.Button(new Rect(mSWidth * 0.7f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "return")){
@@ -151,7 +156,7 @@ public class scriptFarm : MonoBehaviour {
 				Time.timeScale = 1; // resume game
 			}
 		}
-		//in help mode
+		// in help mode
 		else if (mCurState == GameState.HELP) {
 			GUI.Label(new Rect(mSWidth * 0.1f, mSHeight * 0.1f, mSWidth * 0.8f, mSHeight * 0.8f), "Help", mHelpStyle);
 			if(GUI.Button(new Rect(mSWidth * 0.7f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "return")){
@@ -159,15 +164,15 @@ public class scriptFarm : MonoBehaviour {
 				Time.timeScale = 1; // resume game
 			}
 		}
-		//popup
+		// popup
 		if(mShowPopup){
 			string popupText = "";
-			//basic information
+			// basic information
 			popupText += ("ID : " + mTargetRabbit.id + "\n");
 			popupText += ("name : (none)\n");
 			popupText += ("hunger : " + ((mTargetRabbit.hunger != Rabbit.maxHunger + 1) ? mTargetRabbit.hunger.ToString() : "dead") + "\n");
 			popupText += ("gender : " + (mTargetRabbit.grow ? mTargetRabbit.gender.ToString() : "???") + "\n");
-			//add all gene's text in geneList
+			// add all gene's text in geneList
 			for(int i = 0; i < mTargetRabbit.geneList.Count; ++i){
 				popupText += mTargetRabbit.geneList[i].name + " : ";
 				if(mTargetRabbit.grow){
@@ -177,17 +182,17 @@ public class scriptFarm : MonoBehaviour {
 						}
 						popupText += ", ";
 					}
-					//remove last comma
+					// remove last comma
 					popupText = popupText.Remove(popupText.Length - 2, 2);
 				}
-				//don't show genes when rabbit is not grown
+				// don't show genes when rabbit is not grown
 				else{
 					popupText += "???";
 				}
 				popupText += "\n";
 			}
 			GUI.Label (new Rect(mSWidth * 0.75f, mSHeight * 0.0f, mSWidth * 0.25f, mSHeight * 0.5f), popupText, mPopupStyle);
-			//popup close button
+			// popup close button
 			if(GUI.Button (new Rect(mSWidth * 0.925f, mSHeight * 0.025f, mSWidth * 0.05f, mSHeight * 0.05f), "close")){
 				mShowPopup = false;
 			}
