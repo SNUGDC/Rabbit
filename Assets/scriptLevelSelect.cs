775f54741b829e50e2d770bfe55ebf2622263502
Kais-DkM <kais0903@gmail.com>
2014-08-24 00:33:51 +0900

    changed some loading logic

--- Assets/scriptLevelSelect.cs
+++ Assets/scriptLevelSelect.cs
@@ -46,11 +46,47 @@ public class scriptLevelSelect : MonoBehaviour {
 
 	void OnGUI(){
 		if(GUI.Button (new Rect (mSWidth * 0.075f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "1")) {
-			level = 1;
-			GameObject targetRabbit = (GameObject)Instantiate(Resources.Load<GameObject>("prefabRabbit"), new Vector2(-700, 0), Quaternion.identity);
-			targetRabbit.GetComponent<Rabbit>().enabled = false;
-			targetRabbit.GetComponent<Draggable>().enabled = false;
-			targetRabbit.GetComponent<Gene>().create(null, null);
+			load(1);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.225f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "2")) {
+			load(2);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.375f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "3")) {
+			load(3);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.525f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "4")) {
+			load(4);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.675f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "5")) {
+			load(5);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.825f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "6")) {
+			load(6);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.075f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "7")) {
+			load(7);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.225f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "8")) {
+			load(8);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.375f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "9")) {
+			load(9);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.525f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "10")) {
+			load(10);
+		}
+		if(GUI.Button (new Rect (mSWidth * 0.4f, mSHeight * 0.85f, mSWidth * 0.2f, mSHeight * 0.05f), "Back")) {
+			Application.LoadLevel("sceneMainMenu");
+		}
+	}
+
+	private void load(int index){
+		level = index;
+		GameObject targetRabbit = (GameObject)Instantiate(Resources.Load<GameObject>("prefabRabbit"), new Vector2(-700, 0), Quaternion.identity);
+		targetRabbit.GetComponent<Rabbit>().enabled = false;
+		targetRabbit.GetComponent<Draggable>().enabled = false;
+		targetRabbit.GetComponent<Gene>().create(null, null);
+		if(levelList[level - 1].target != null){
 			for(int i = 0; i < levelList[level - 1].target[0].Length; ++i){
 				for(int j = 0; j < levelList[level - 1].target[i + 1].Length; j += 2){
 					targetRabbit.GetComponent<Gene>().setField(levelList[level - 1].target[0][i],
@@ -59,57 +95,8 @@ public class scriptLevelSelect : MonoBehaviour {
 															   levelList[level - 1].target[i + 1][j + 1]);
 				}
 			}
-			geneList = targetRabbit.GetComponent<Gene>().list;
-			Application.LoadLevel("sceneFarm");
 		}
-		if(GUI.Button (new Rect (mSWidth * 0.225f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "2")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.375f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "3")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.525f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "4")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.675f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "5")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.825f, mSHeight * 0.1f, mSWidth * 0.1f, mSHeight * 0.1f), "6")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.075f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "7")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.225f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "8")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.375f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "9")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.525f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "10")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.675f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "11")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.825f, mSHeight * 0.3f, mSWidth * 0.1f, mSHeight * 0.1f), "12")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.075f, mSHeight * 0.5f, mSWidth * 0.1f, mSHeight * 0.1f), "13")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.225f, mSHeight * 0.5f, mSWidth * 0.1f, mSHeight * 0.1f), "14")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.375f, mSHeight * 0.5f, mSWidth * 0.1f, mSHeight * 0.1f), "15")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.525f, mSHeight * 0.5f, mSWidth * 0.1f, mSHeight * 0.1f), "16")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.675f, mSHeight * 0.5f, mSWidth * 0.1f, mSHeight * 0.1f), "17")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.825f, mSHeight * 0.5f, mSWidth * 0.1f, mSHeight * 0.1f), "18")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.075f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "19")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.225f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "20")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.375f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "21")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.525f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "22")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.675f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "23")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.825f, mSHeight * 0.7f, mSWidth * 0.1f, mSHeight * 0.1f), "24")) {
-		}
-		if(GUI.Button (new Rect (mSWidth * 0.4f, mSHeight * 0.85f, mSWidth * 0.2f, mSHeight * 0.05f), "Back")) {
-			Application.LoadLevel("sceneMainMenu");
-		}
+		geneList = targetRabbit.GetComponent<Gene>().list;
+		Application.LoadLevel("sceneFarm");
 	}
 }
