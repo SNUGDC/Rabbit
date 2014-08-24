e90ba01dc23d4f3944c4fb14b1d8dc309486f22b
yulcat <alszmtm@naver.com>
2014-08-24 16:04:01 +0900

    Hierachy Modified

---
+++ Assets/RabbitSprite.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RabbitSprite : MonoBehaviour {
+	[System.Serializable]
+	public class Spritelist{
+		public Sprite[] sprites = new Sprite[5];
+		public Sprite this[int i]
+		{
+			get
+			{
+				return sprites[i];
+			}
+			set
+			{
+				sprites[i] = value;
+			}
+		}
+	}
+
+	public Spritelist[] headList = new Spritelist[3];
+	public Spritelist[] bodyList = new Spritelist[4];
+	public Sprite[] tailList = new Sprite[5];
+	public Sprite[] teethList = new Sprite[2];
+
+	public static RabbitSprite manager;
+	// Use this for initialization
+	void Awake () {
+		manager = this;
+	}
+}
